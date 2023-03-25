4c3458c0690b566663bd9809cf32ce78ebc43178
Glen <glen.kimani@student.moringaschool.com>
2023-03-26 00:26:35 +0300

    Commit

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,2 +1,21 @@
 class HeroesController < ApplicationController
+
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+
+    def index
+        @hero = Hero.all
+        render json: @hero, status: :ok
+    end
+
+    def show
+        @hero = Hero.find(params[:id])
+        render json: @hero, serializer: HeroAndPowerSerializer,  status: :ok
+    end
+
+    private
+
+    def not_found_response
+        render json: { error: "Hero not found" }, status: :not_found
+    end
+
 end
